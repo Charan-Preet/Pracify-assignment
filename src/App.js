5bdd20f7045a7c7fc1821756dbd7780695a9470f
CharanPreet <57750440+charanpreet-byte@users.noreply.github.com>
2021-05-30 07:49:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='navbar'>
+        <div className='organic'>
+          
+          organic
+         </div>
+        <div className='nav-link'>
+          Home
+          Products
+          Blog
+          About Us
+          Contact
+       </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
